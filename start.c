71168087a6127ccd56f4a75c61d8d6c0cd7e5514
AKRAMELGYAR <akramelgyar@gmail.com>
2023-05-23 11:42:41 -0700

    test 10

---
+++ start.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "text.h"
+#include "general.h"
+
+/**
+ * start - H kxvcb kj kb xc kb xck jbn x bkxb kzcxbkjcucxhxhvzu
+ * Description: Mode can be INTE kbvkjcbvjkxcbck bjkxchuvskhvkn xjkcvb
+ *
+ * @info: Struct of information about the shell
+ **/
+void start(general_t *info)
+{
+	start_prompt(info);
+}
