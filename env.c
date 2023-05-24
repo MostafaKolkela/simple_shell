4931acb2b8fd7e91dd52db560b774dad8f67a386
AKRAMELGYAR <akramelgyar@gmail.com>
2023-05-24 06:30:02 -0700

    test s

--- env.c
+++ env.c
@@ -1,8 +1,11 @@
 #include "builtins.h"
 
 /**
- mostafa
- **/
+ * bin_env_ njdkfhkfjvsld vjndsl ,vlsavkbb d hkbv snkxbvbk
+ * Descripation: kjfhvedbvkbev kdbv dsfn kdv  dk d vmsbd  d jvsabhbjs
+ * @info: hkfebvkbufduvisukgvydgw7yw
+ * @@arguments : djfksbvkjvdsohhjf
+*/
 void bin_env(general_t *info, char **arguments)
 {
 	(void) info;
